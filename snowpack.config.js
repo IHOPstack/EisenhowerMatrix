458c9790934e83d26ac0cf1b58922206b965c17b
Devin Beckwith <beckwithdevin@yahoo.com>
2024-07-10 15:44:36 -0700

    Adjust build settings

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,29 +1,7 @@
 module.exports = {
     mount: {
-      public: '/public',
+      public: '/',
       src: '/'
-    },
-    plugins: [
-      /* Add any Snowpack plugins here */
-    ],
-    routes: [
-    ],
-    optimize: {
-      /* Enable minification */
-      minify: true,
-      /* Enable tree-shaking */
-      treeshake: true,
-      /* Create a single bundle */
-      bundle: true
-    },
-    packageOptions: {
-      /* Add any package options here */
-    },
-    devOptions: {
-      /* Add any development options here */
-    },
-    buildOptions: {
-      /* Add any build options here */
-    },
+    }
   };
   
