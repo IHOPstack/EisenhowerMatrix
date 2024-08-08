a0b58a48296e8de4456befa38a88703b91d15593
Devin Beckwith <beckwithdevin@yahoo.com>
2024-08-08 14:45:25 -0700

    Removed fixed grid size relics

--- public/js/organizer.js
+++ public/js/organizer.js
@@ -1,13 +1,9 @@
 import { getCardPriority } from './utils.js';
 
-const gridRows = 7;
-const gridCols = 7;
-
 function calculateSortScore(importance, urgency) {
-  const maxScore = gridRows + gridCols - 2;
   const diagonalScore = urgency + importance;
   const adjustedScore = diagonalScore * 1000 + urgency;
-  return maxScore - adjustedScore;
+  return - adjustedScore;
 }
 
 export async function sortCards(cards, t) {
@@ -19,6 +15,5 @@ export async function sortCards(cards, t) {
     })
   );
 
-  // Change the sort order here
   return cardsWithScores.sort((a, b) => a.score - b.score);
 }
